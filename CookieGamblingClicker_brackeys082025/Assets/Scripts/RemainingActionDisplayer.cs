9c5b15af44a8e78e5fc406e56184d3ae31266ec2
AcreeXer <alexandrecasteels@gmail.com>
2025-08-25 23:05:50 +0200

    add Burning Candle as Remaining Action count

--- CookieGamblingClicker_brackeys082025/Assets/Scripts/RemainingActionDisplayer.cs
+++ CookieGamblingClicker_brackeys082025/Assets/Scripts/RemainingActionDisplayer.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -6,16 +9,24 @@ namespace CookieGambler
     public class RemainingActionDisplayer : MonoBehaviour
     {
         [SerializeField]
-        private TextMeshProUGUI remainingActionText;
+        private Vector3 CandleSizeFactor = new Vector3(0f, 0.8f, 0f);
+
         [SerializeField]
-        private int remainingActionCount = 5;
+        private GameObject candleBody;
+        [SerializeField]
+        private TMP_Text remainingActionText;
+        [SerializeField]
+        private int remainingActionMax = 5;
+        [SerializeField]
+        private Light[] allLights;
+        private int remainingActionCount;
 
         public int RemainingActionCount
         {
             get => remainingActionCount;
             set
             {
-                remainingActionCount = Mathf.Min(value, 0);
+                remainingActionCount = Mathf.Clamp(value, 0, remainingActionMax);
 
                 if (remainingActionCount <= 0)
                 {
@@ -26,14 +37,44 @@ namespace CookieGambler
             }
         }
 
+        public int RemainingActionMax
+        {
+            get => remainingActionMax;
+            set
+            {
+                remainingActionMax = value;
+                remainingActionCount = remainingActionMax;
+            }
+        }
+
         private void Start()
         {
+            RemainingActionMax = remainingActionMax;
             UpdateRemainingAction();
         }
 
         private void UpdateRemainingAction()
         {
-            remainingActionText.text = "Ramaining Action: " + remainingActionCount;
+            remainingActionText.text = RemainingActionCount.ToString();
+            StartCoroutine(ConsumeCandle());
+        }
+
+        private IEnumerator ConsumeCandle()
+        {
+            Vector3 initScale = candleBody.transform.localScale;
+            Vector3 finalScale = Vector3.one + (CandleSizeFactor * (1f - RemainingActionCount / (float)RemainingActionMax));
+
+            float time = 0;
+            while(Vector3.Distance(candleBody.transform.localScale, finalScale) > Mathf.Epsilon)
+            {
+                time += Time.deltaTime;
+                candleBody.transform.localScale = Vector3.Lerp(initScale, finalScale, time);
+                foreach (var light in allLights)
+                {
+                    light.intensity *= 0.999f;
+                }
+                yield return null;
+            }
         }
     }
 }
